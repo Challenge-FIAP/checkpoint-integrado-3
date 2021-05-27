ffb9344849fd6f643687b4ad8e93b2d5fd6565ed
Augusto Seabra <augusto.seabra00@gmail.com>
2021-05-27 14:33:30 -0300

    Adição de pattern, junção do JS e realização de testes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+/* 
+ * Nome dos integrantes:
+ * Augusto Seabra - RM 87841;
+ * Matheus Camargo Xavier - RM 86571;
+ * Marcos Henrique da Silva - RM 88470;
+ * Vitor Machado de Campos - RM 87229.
+ */
+
 const usuariosCadastrados = [
   { nome: "Augusto", senha: "87841" },
   { nome: "Matheus", senha: "86571" },
@@ -6,6 +14,14 @@ const usuariosCadastrados = [
 ]
 
 function validar(nome, senha) {
+  nome = retornaValorFormatado(nome)
+  senha = retornaValorFormatado(senha)
+
+  if(verificarValorEstaVazio(nome) || verificarValorEstaVazio(senha)) {
+    alert("Preencha corretamente os campos.")
+    return false
+  }
+
   let estaCadastrado = false
   for(let i = 0; i < usuariosCadastrados.length; i++) {
     if(estaCadastrado) continue
@@ -16,14 +32,21 @@ function validar(nome, senha) {
 }
 
 function cadastrar(nome, senha) {
-  const todasSenhas = usuariosCadastrados.map(({ senha }) => { return senha })
-  if(todasSenhas.includes(senha)) {
+  nome = retornaValorFormatado(nome)
+  senha = retornaValorFormatado(senha)
+  if(verificarValorEstaVazio(nome) || verificarValorEstaVazio(senha)) {
+    alert("Verifique se os campos de nome e senha estão corretamente preenchidos.")
+    return false
+  }
+
+  const todosNomes = usuariosCadastrados.map(({ nome }) => { return nome })
+  if(todosNomes.includes(nome)) {
     alert(`${nome} já foi cadastrado!`)
     return false
   }
 
   usuariosCadastrados.push({ nome, senha })
-  alert(`${nome} foi inserido!\n Confira os outros usuários cadastrados: ${mostrarTodosNomesCadastrados()}`)
+  alert(`${nome} foi inserido!\n Confira os outros usuários cadastrados:\n ${mostrarTodosNomesCadastrados()}`)
   window.location.href = "./portal.html"
 }
 
@@ -38,7 +61,11 @@ function mostrarTodosNomesCadastrados() {
 }
 
 function validarDados(assunto, nome, email, comentario) {
-  if(!(assunto && nome && email && comentario)) {
+  assunto = retornaValorFormatado(assunto)
+  nome = retornaValorFormatado(nome)
+  email = retornaValorFormatado(email)
+  comentario = retornaValorFormatado(comentario)
+  if(verificarValorEstaVazio(assunto) || verificarValorEstaVazio(nome) || verificarValorEstaVazio(email) || verificarValorEstaVazio(comentario)) {
     alert("Por favor verifique os dados enviados.")
   }else {
     alert("Todos campos foram preenchidos!")
@@ -46,10 +73,10 @@ function validarDados(assunto, nome, email, comentario) {
   }
 }
 
-/*
- * Nome dos integrantes -
- * Augusto Seabra - RM 87841;
- * Matheus Camargo Xavier - RM 86571;
- * Marcos Henrique da Silva - RM 88470;
- * Vitor Machado de Campos - RM 87229.
- */
+function retornaValorFormatado(valor){
+  return valor.trim("")
+}
+
+function verificarValorEstaVazio(valor) {
+  return /^\s*$/.test(valor)
+}
